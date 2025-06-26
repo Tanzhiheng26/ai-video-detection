b829195e3a8b65c0417a061678fe484c44ab358b
Zhi Heng <tanzhiheng26@gmail.com>
2025-06-26 16:30:09 +0800

    Update utility functions

--- videomae/utils.py
+++ videomae/utils.py
@@ -10,23 +10,23 @@ def unnormalize_img(img, image_processor):
     img = (img * 255).astype("uint8")
     return img.clip(0, 255)
 
-def create_gif(video_tensor, filename="sample.gif"):
+def create_gif(video_tensor, image_processor, filename="sample.gif"):
     """Prepares a GIF from a video tensor.
     The video tensor is expected to have the following shape:
     (num_frames, num_channels, height, width).
     """
     frames = []
     for video_frame in video_tensor:
-        frame_unnormalized = unnormalize_img(video_frame.permute(1, 2, 0).numpy())
+        frame_unnormalized = unnormalize_img(video_frame.permute(1, 2, 0).numpy(), image_processor)
         frames.append(frame_unnormalized)
     kargs = {"duration": 0.25}
     imageio.mimsave(filename, frames, "GIF", **kargs)
     return filename
 
-def display_gif(video_tensor, gif_name="sample.gif"):
+def display_gif(video_tensor, image_processor, gif_name="sample.gif"):
     """Prepares and displays a GIF from a video tensor."""
     video_tensor = video_tensor.permute(1, 0, 2, 3)
-    gif_filename = create_gif(video_tensor, gif_name)
+    gif_filename = create_gif(video_tensor, image_processor, gif_name)
     return Image(filename=gif_filename)
 
 def collate_fn(examples):
